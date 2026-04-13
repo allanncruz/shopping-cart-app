d24516bdaceb669388ba0087e95b5e5bbda1d73f
Allan <allancruz716@gmail.com>
2026-04-13 13:03:30 -0300

    feat: list data post

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,33 @@
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
+import { getPosts } from "../services/postApi";
+import { Post } from "../types/post";
 
 export default function Index() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const response = await getPosts();
+        setPosts(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    load();
+  }, []);
+
   return (
-    <View>
-      <Text>Initial app</Text>
-    </View>
+    <FlatList
+      data={posts}
+      keyExtractor={(item, index) => String(item.id ?? index)}
+      renderItem={({ item }) => (
+        <View>
+          <Text>{item.title}</Text>teste
+        </View>
+      )}
+    />
   );
 }
