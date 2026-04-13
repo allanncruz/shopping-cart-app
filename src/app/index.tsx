b1602e9d0a4eccd94f26073be9dc7abbff277fcf
Allan <allancruz716@gmail.com>
2026-04-13 16:02:07 -0300

    chore: config use navigation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,9 +4,20 @@ import { ProductCard } from "../components/ProductsList";
 import { getProducts } from "../services/productsApi";
 import { Product } from "../types/products";
 
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types/navigation";
+
+type NavigationProps = NativeStackNavigationProp<
+  RootStackParamList,
+  "ProductList"
+>;
+
 export default function Index() {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const navigation = useNavigation<NavigationProps>();
 
   useEffect(() => {
     const load = async () => {
